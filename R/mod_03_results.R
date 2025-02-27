0c8b50870e3a6916eb6761b28a65ce5cc7b83566
Simon Wellesley-Miller <58369769+Simon-W-M@users.noreply.github.com>
2025-02-27 17:47:35 +0000

    updated charts, exploring tables
    stable work in progress showing charts of outputs

--- R/mod_03_results.R
+++ R/mod_03_results.R
@@ -10,6 +10,7 @@
 #' @importFrom DT DTOutput
 #' @importFrom bslib navset_tab nav_panel card card_body
 #' @importFrom dplyr if_else
+#' @importFrom tidyr pivot_wider
 mod_03_results_ui <- function(id){
   ns <- NS(id)
   tagList(
@@ -25,23 +26,41 @@ mod_03_results_ui <- function(id){
         )
       ),
       nav_panel(
-        title = "One",
+        title = "WL Size",
         p("Second tab content."),
          plotOutput(
            ns("wl_size"),
            click = "plot_click"
-         )
+         ),
+        DT::DTOutput(
+          ns("wl_size_table")
+        )
       ),
       nav_panel(
-        title = "Two",
+        title = "WL 4mth Performance",
         p("Second tab content."),
         plotOutput(
-          ns("wl_perf"),
+          ns("wl_performance"),
           click = NULL
                 )
         ),
-      nav_panel(title = "Three", p("Second tab content.")),
+      nav_panel(
+        title = "WL Reneges",
+        p("Second tab content."),
+        plotOutput(
+          ns("wl_reneging_plot"),
+          click = NULL
+        )
+      ),
       nav_panel(title = "Four", p("Second tab content.")),
+      nav_panel(
+        title = "WL Capacity",
+        p("Second tab content."),
+        plotOutput(
+          ns("wl_capacity"),
+          click = NULL
+        )
+      ),
       nav_panel(title = "Five", p("Second tab content."))
     )
   )
@@ -61,65 +80,163 @@ mod_03_results_server <- function(id, r){
 
     })
 
-    ## Create waiting list plots here
+    ## Create waiting list size plot(s)
     output$wl_size <- renderPlot({
       dat<- r$waiting_list |>
         dplyr::summarise(tot = sum(incompletes, na.rm = T),
-                         .by = period)
+                         .by = c(period, period_type))
 
-     ggplot2::ggplot(dat, aes(x = period,
-                                y = tot)) +
-       geom_point() +
-       geom_line() +
-       theme_minimal()
+      # determine skew applied from model
+      skews <- unique(r$waiting_list$capacity_skew)
+      if (length(skews)== 1) {cap_skew <- 1} else {cap_skew <- skews[skews!=1]}
+
+      p <- ggplot2::ggplot() +
+        geom_line(data = dplyr::filter(dat, period_type == 'Observed'),
+                  aes(x = period,
+                      y = tot,
+                      group = 1
+                  ),  colour = 'black',
+                  show.legend = T) +
+        geom_line(data = dplyr::filter(dat, period_type == 'Projected'),
+                  aes(x = period,
+                      y = tot,
+                      group = 2
+                  ),
+                  colour = 'blue',
+                  linetype = 'dashed') +
+        theme_minimal() +
+        ylab('Total patients waiting') +
+        xlab(NULL) +
+        scale_x_date(breaks = "3 month", minor_breaks = "1 month", date_labels = "%b %y" ) +
+        labs(title = paste0 ('Observed and projected waiting list size: ', format(min(dat$period),'%b %Y'), '-', format(max(dat$period),'%b %Y')),
+             subtitle = paste0('Based on skew of ', cap_skew))
+
+      p
    }, res = 96)
 
-    ## Create perfromance plots here
-    output$wl_perf <- renderPlot({
-      # dat2<- r$waiting_list |>
-      #   dplyr::summarise(tot = sum(incompletes, na.rm = T),
-      #                  .by = c(period, months_waited_id)) |>
-      #   dplyr::mutate(test_col = dplyr::if_else (months_waited_id <= 3, 1, 0))
-      #
-      # ggplot2::ggplot(dat2, aes(x = period,
-      #                          y = tot)) +
-      #   geom_point() +
-      #   geom_line() +
-      #   theme_minimal()
 
-      # dat2$target_flag <- ifelse (dat2$month_waited_id <= 3, 1, 0)
-      #
-      # dat2 <- dat2 |>
-      #       dplyr::summarise(tot_wait = sum(incompletes),
-      #                        .by = c(target_flag, period)) |>
-      #       dplyr::mutate(perf = tot_wait / sum(tot_wait),
-      #                     .by = c(period)) |>
-      #       dplyr::filter(target_flag == 1)
-      #
+    output$wl_size_table <- DT::renderDT({
 
+      r$waiting_list |>
+        dplyr::arrange (period) |>
+        dplyr::mutate(period = format(period,'%m-%y')) |>
+        dplyr::summarise(tot = round(sum(incompletes, na.rm = T),0),
+                         .by = c(period)) |>
+        tidyr::pivot_wider(names_from = period, values_from = tot)
+
+    })
+
+    ## Create performance plots here
+    output$wl_performance <- renderPlot({
+      perf <- r$waiting_list |>
+          dplyr::mutate(target_flag = dplyr::if_else (months_waited_id <= 3, 1, 0)) |>
+          dplyr::summarise(tot_wait = sum(incompletes),
+                           .by = c(target_flag, period, period_type)) |>
+          dplyr::mutate(perf_perc = tot_wait / sum(tot_wait),
+                        .by = c(period, period_type)) |>
+          dplyr::filter(target_flag == 1)
 
 
-        # ggplot2::ggplot(dat2, aes(x = period,
-        #                        y = tot)) +
-        # geom_point() +
-        # geom_line() +
-        # theme_minimal()
+      ggplot2::ggplot() +
+        geom_line(
+          data = dplyr::filter(perf, period_type == "Observed"),
+          aes(x = period,
+              y = perf_perc,
+              group = 1),
+          colour = "black") +
+        geom_line(
+          data = dplyr::filter(perf, period_type == "Projected"),
+          aes(x = period,
+              y = perf_perc,
+              group = 2),
+          colour = "blue",
+          linetype = "dashed"
+          ) +
+        theme_minimal() +
+        scale_y_continuous(labels = scales::percent) +
+        scale_x_date(breaks = "3 month",
+                     minor_breaks = "1 month",
+                     date_labels = "%b %y") +
+        ylab("4 Months performance achievement") +
+        xlab(NULL) +
+        labs(
+          title = paste0("Observed and projected 4 month performance: ", format(min(d$period), "%b %Y"), "-", format(max(d$period), "%b %Y")),
+          subtitle = paste0("Based on "),
+          caption = paste0("Data taken from blah blah on ", format(Sys.Date(), "%d/%m/%Y"))
+        )
 
-      #
-      # per <- r$waiting_list |>
-      #     dplyr::mutate(target_flag = dplyr::if_else (month_waited_id <= 3, 1, 0)) |>
-      #     dplyr::summarise(tot_wait = sum(incompletes),
-      #                      .by = c(target_flag, period)) |>
-      #     dplyr::mutate(perf = tot_wait / sum(tot_wait),
-      #                   .by = c(period)) |>
-      #     dplyr::filter(target_flag == 1)
+    }, res = 96)
 
-#
-#       ggplot2::ggplot(performance, aes(x= period, y = perf)) +
-#         geom_point() +
-#         geom_line() +
-#         theme_minimal() +
-#         scale_y_continuous(labels = scales::percent)
+    ## Create reneging plots
+    output$wl_reneging_plot <- renderPlot({
+
+      dat<- r$waiting_list |>
+        dplyr::summarise(tot = sum(reneges, na.rm = T),
+                         .by = c(period, period_type, months_waited_id))
+
+      # determine skew applied from model
+      skews <- unique(r$waiting_list$capacity_skew)
+      if (length(skews)== 1) {cap_skew <- 1} else {cap_skew <- skews[skews!=1]}
+
+      p <- ggplot2::ggplot() +
+        geom_line(data = dplyr::filter(dat, period_type == 'Observed'),
+                  aes(x = period,
+                      y = tot,
+                      group = 1
+                  ),  colour = 'black',
+                  show.legend = T) +
+        geom_line(data = dplyr::filter(dat, period_type == 'Projected'),
+                  aes(x = period,
+                      y = tot,
+                      group = 2
+                  ),
+                  colour = 'blue',
+                  linetype = 'dashed') +
+        theme_minimal() +
+        facet_wrap(~months_waited_id, ncol = 4) +
+        ylab('Total patients waiting') +
+        xlab(NULL) +
+        scale_x_date(breaks = "3 month", minor_breaks = "1 month", date_labels = "%b %y" ) +
+        labs(title = paste0 ('Observed and projected reneges: ', format(min(dat$period),'%b %Y'), '-', format(max(dat$period),'%b %Y')),
+             subtitle = paste0('Based on skew of ', cap_skew))
+
+      p
+
+
+    }, res = 96)
+
+    ## Create waiting list capacity plot(s)
+    output$wl_capacity <- renderPlot({
+      dat<- r$waiting_list |>
+        dplyr::summarise(tot = sum(calculated_treatments, na.rm = T),
+                         .by = c(period, period_type))
+
+      # determine skew applied from model
+      skews <- unique(r$waiting_list$capacity_skew)
+      if (length(skews)== 1) {cap_skew <- 1} else {cap_skew <- skews[skews!=1]}
+
+      p <- ggplot2::ggplot() +
+        geom_line(data = dplyr::filter(dat, period_type == 'Observed'),
+                  aes(x = period,
+                      y = tot,
+                      group = 1
+                  ),  colour = 'black',
+                  show.legend = T) +
+        geom_line(data = dplyr::filter(dat, period_type == 'Projected'),
+                  aes(x = period,
+                      y = tot,
+                      group = 2
+                  ),
+                  colour = 'blue',
+                  linetype = 'dashed') +
+        theme_minimal() +
+        ylab('Total patients waiting') +
+        xlab(NULL) +
+        scale_x_date(breaks = "3 month", minor_breaks = "1 month", date_labels = "%b %y" ) +
+        labs(title = paste0 ('Observed and projected completed pathways: ', format(min(dat$period),'%b %Y'), '-', format(max(dat$period),'%b %Y')),
+             subtitle = paste0('Based on skew of ', cap_skew))
+
+      p
     }, res = 96)
 
   })
