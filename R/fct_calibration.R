8ec3d32dacc924077c6696347de0e200d110be76
Fox Sebastian (BNSSG CCG) <Sebastian.Fox@BNSSGCCG.NHS.UK>
2025-03-05 11:39:14 +0000

    adds tests for calibration functions

--- R/fct_calibration.R
+++ R/fct_calibration.R
@@ -5,13 +5,66 @@
 #'   type, months_waited_id, value
 #' @param max_months_waited integer; the stock to pool the stocks that have
 #'   waited longer into
-#' @param referrals_uplift numeric; multiplier for referral inputs (calculated
-#'   from negative renege_params when calibrating the models). These occur due
-#'   to under-reporting of referrals data
+#' @param referrals_uplift numeric; single value - parameter to apply to
+#'   referral inputs (absolute value of the renege_params in the first stock
+#'   when calibrating the models). These occur due to under-reporting of
+#'   referrals data. This is applied to the observed referrals using the
+#'   following formula:
+#'
+#'   \deqn{referrals_{adjusted} = referrals_{obs} + (referrals_{obs} *
+#'   uplift\_parameter)}
+#'
+#'   See details for more information on this argument and how it is applied.
+#'
+#' @details This is the maths for stock = 0. We know:
+#'
+#'
+#'   (Equation 1) \deqn{incomplete_{obs} = referrals_{obs} - complete_{obs} -
+#'   reneges_{calc}}
+#'
+#'   BUT, when \eqn{reneges_{calc}} are negative, we want to adjust referrals by
+#'   that amount
+#'
+#'   \deqn{incompletes_{obs} = referrals\_adj_{calc} - complete_{obs}}
+#'
+#'   WHERE
+#'
+#'   (Equation 2) \deqn{referrals\_adj_{calc} = referrals_{obs} +
+#'   reneges_{calc}} (where \eqn{reneges_{calc}} is from equation 1)
+#'
+#'   ALSO
+#'
+#'   (Equation 3)
+#'
+#'   \deqn{renege\_param_{calc} = \frac{reneges_{calc}}{referrals_{obs}}}
+#'
+#'   THEREFORE, combining eq. 2 and eq. 3 (substituting \eqn{reneges_{calc}})
+#'
+#'   (Equation 4) \deqn{renege\_param_{calc} = \frac{referrals\_adj_{calc} -
+#'   referrals_{obs}}{referrals_{obs}}}
+#'
+#'   REARRANGING eq. 4
+#'
+#'   \deqn{referrals\_adj_{calc} = (renege\_param_{calc} * referrals_{obs}) +
+#'   referrals_{obs}}
+#'
 #'
 #' @importFrom dplyr filter distinct rename left_join join_by
 #' @importFrom tidyr complete nest
 create_modelling_data <- function(data, max_months_waited = 12, referrals_uplift) {
+
+  required_fields <- c(
+    "trust",
+    "specialty",
+    "period_id",
+    "type",
+    "months_waited_id",
+    "value"
+  )
+
+  if (length(dplyr::setdiff(required_fields, names(data)) > 0))
+    stop("incorrect fields in data object - requires 'trust', 'specialty', 'period_id', 'type', 'months_waited_id', 'value'")
+
   periods <- unique(
     sort(
       data$period_id
@@ -51,6 +104,8 @@ create_modelling_data <- function(data, max_months_waited = 12, referrals_uplift
     )
 
   if (!is.null(referrals_uplift)) {
+
+
     referrals <- referrals |>
       dplyr::mutate(
         referrals = referrals + (referrals * referrals_uplift)
@@ -60,8 +115,8 @@ create_modelling_data <- function(data, max_months_waited = 12, referrals_uplift
   referrals <- referrals |>
     tidyr::nest(
       referrals_data = c(
-        .data$period_id,
-        .data$referrals
+        "period_id",
+        "referrals"
       )
     )
 
@@ -88,9 +143,9 @@ create_modelling_data <- function(data, max_months_waited = 12, referrals_uplift
     ) |>
     tidyr::nest(
       completes_data = c(
-        .data$period_id,
-        .data$months_waited_id,
-        .data$treatments
+        "period_id",
+        "months_waited_id",
+        "treatments"
       )
     )
 
@@ -118,9 +173,9 @@ create_modelling_data <- function(data, max_months_waited = 12, referrals_uplift
     ) |>
     tidyr::nest(
       incompletes_data = c(
-        .data$period_id,
-        .data$months_waited_id,
-        .data$incompletes
+        "period_id",
+        "months_waited_id",
+        "incompletes"
       )
     )
 
--- tests/testthat/test-fct_calibration.R
+++ tests/testthat/test-fct_calibration.R
@@ -1,3 +1,173 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("create_modelling_data works", {
+
+  top_bin <- 12
+  periods <- 6
+
+  df <- c("referral", "incomplete", "complete") |>
+    purrr::map(
+      ~ NHSRtt::create_dummy_data(
+        type = .x,
+        max_months_waited = top_bin,
+        number_periods = periods,
+        seed  = 444
+      )
+    ) |>
+    purrr::list_rbind() |>
+    dplyr::mutate(
+      months_waited_id = case_when(
+        !is.na(referrals) ~ 0,
+        .default = months_waited_id
+      ),
+      value = case_when(
+        !is.na(referrals) ~ referrals,
+        !is.na(incompletes) ~ incompletes,
+        !is.na(treatments) ~ treatments,
+        .default = NA_real_
+      ),
+      type = case_when(
+        !is.na(referrals) ~ "Referrals",
+        !is.na(incompletes) ~ "Incomplete",
+        !is.na(treatments) ~ "Complete",
+        .default = NA_character_
+      ),
+      trust = "RXX",
+      specialty = "C_XXX"
+    ) |>
+    select(!c("referrals", "incompletes", "treatments"))
+
+
+  modelling_df <- create_modelling_data(
+    df,
+    max_months_waited = top_bin,
+    referrals_uplift = 0.1
+  )
+
+
+  expect_equal(
+    nrow(modelling_df),
+    1,
+    info = "1 row returned from create_modelling_data function"
+  )
+
+  expect_equal(
+    names(modelling_df),
+    c("specialty", "trust", "completes_data", "referrals_data", "incompletes_data"),
+    info = "names of create_modelling_data outputs are expected"
+  )
+
+
+  expect_equal(
+    df |>
+      filter(type == "Referrals") |>
+      pull(value) |>
+      (\(x) x + (x * 0.1))(),
+    modelling_df$referrals_data[[1]]$referrals,
+    info = "referrals uplift works as expected"
+  )
+
+  expect_equal(
+    nrow(modelling_df$completes_data[[1]]),
+    length(unique(df$period_id)) * length(unique(df$months_waited_id)),
+    info = "correct number of records in completes_data"
+  )
+
+  expect_equal(
+    nrow(modelling_df$incompletes_data[[1]]),
+    length(unique(df$period_id)) * length(unique(df$months_waited_id)),
+    info = "correct number of records in incompletes_data"
+  )
+
+  expect_equal(
+    sapply(modelling_df, class),
+    c(specialty = "character",
+      trust = "character",
+      completes_data = "list",
+      referrals_data = "list",
+      incompletes_data = "list"
+    ),
+    info = "class of modelling_df fields are correct"
+  )
+})
+
+
+test_that("calibrate_parameters works", {
+
+  top_bin <- 12
+  periods <- 6
+
+  df <- c("referral", "incomplete", "complete") |>
+    purrr::map(
+      ~ NHSRtt::create_dummy_data(
+        type = .x,
+        max_months_waited = top_bin,
+        number_periods = periods,
+        seed  = 444
+      )
+    ) |>
+    purrr::list_rbind() |>
+    dplyr::mutate(
+      months_waited_id = case_when(
+        !is.na(referrals) ~ 0,
+        .default = months_waited_id
+      ),
+      value = case_when(
+        !is.na(referrals) ~ referrals,
+        !is.na(incompletes) ~ incompletes,
+        !is.na(treatments) ~ treatments,
+        .default = NA_real_
+      ),
+      type = case_when(
+        !is.na(referrals) ~ "Referrals",
+        !is.na(incompletes) ~ "Incomplete",
+        !is.na(treatments) ~ "Complete",
+        .default = NA_character_
+      ),
+      trust = "RXX",
+      specialty = "C_XXX"
+    ) |>
+    select(!c("referrals", "incompletes", "treatments"))
+
+
+  params <- calibrate_parameters(
+    rtt_data = df,
+    max_months_waited = top_bin,
+    redistribute_m0_reneges = TRUE,
+    referrals_uplift = 0,
+    full_breakdown = FALSE
+  )
+
+  expect_equal(
+    nrow(params$params[[1]]),
+    top_bin + 1,
+    info = "params generated for each bin"
+  )
+
+  expect_equal(
+    names(params$params[[1]]),
+    c("months_waited_id", "renege_param", "capacity_param"),
+    info = "expected names in params output"
+  )
+
+  params_full <- calibrate_parameters(
+    rtt_data = df,
+    max_months_waited = top_bin,
+    redistribute_m0_reneges = TRUE,
+    referrals_uplift = 0,
+    full_breakdown = TRUE
+  )
+
+  expect_equal(
+    nrow(params_full$params[[1]]),
+    (top_bin + 1) * periods,
+    info = "params generated for each bin and time period in full breakdown params output"
+  )
+
+  expect_equal(
+    names(params_full$params[[1]]),
+    c("period_id", "months_waited_id", "node_inflow", "treatments",
+      "waiting_same_node", "reneges", "renege_param", "capacity_param"),
+    info = "expected names in full breakdown params output"
+  )
+
+
 })
