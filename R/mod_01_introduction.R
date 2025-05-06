2454c1260420b3a5afb9d76064eb06785e92128b
Fox Sebastian (BNSSG CCG) <Sebastian.Fox@BNSSGCCG.NHS.UK>
2025-05-06 10:02:57 +0100

    creates temporary landing page for main branch

--- R/mod_01_introduction.R
+++ R/mod_01_introduction.R
@@ -4,28 +4,36 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
+#' @importFrom bslib page_fluid card card_header card_body
 mod_01_introduction_ui <- function(id){
   ns <- NS(id)
-  tagList(
- 
+  page_fluid(
+    card(
+      card_header(
+        "Tool release information"
+      ),
+      card_body(
+        "This tool is under development and will be released in early May 2025."
+      )
+    )
   )
 }
-    
+
 #' 01_introduction Server Functions
 #'
-#' @noRd 
+#' @noRd
 mod_01_introduction_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
   })
 }
-    
+
 ## To be copied in the UI
 # mod_01_introduction_ui("01_introduction_1")
-    
+
 ## To be copied in the server
 # mod_01_introduction_server("01_introduction_1")
